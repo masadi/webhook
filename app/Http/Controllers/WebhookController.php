5357288c98857d2c0bc8d577321c3c80e582202c
masadi <masadi.com@gmail.com>
2024-02-29 13:57:09 +0700

    update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
     public function index(){
+        $rawdata = file_get_contents("php://input");
+		$json = json_decode($rawdata, true);
         Storage::disk('public')->put('whatsapp.json', json_encode(request()->all()));
+        Storage::disk('public')->put('rawdata.json', $json);
         Log::build([
             'driver' => 'single',
             'path' => storage_path('logs/whatsapp.log'),
